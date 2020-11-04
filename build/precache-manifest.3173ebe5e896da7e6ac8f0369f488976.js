f72967c1991e2879fe6c082b055aa2aa0686b5d1
Chicodemilo <mileschick@gmail.com>
2020-11-04 12:10:27 -0600

    another new build

---
+++ build/precache-manifest.3173ebe5e896da7e6ac8f0369f488976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f1681216e2f250df7b01ac4c53b06a",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "3d2dcc184fd3aa680393",
+    "url": "/build/static/css/main.a2de57e9.chunk.css"
+  },
+  {
+    "revision": "5de28426551fce9d59b3",
+    "url": "/build/static/js/2.10faa756.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/build/static/js/2.10faa756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2dcc184fd3aa680393",
+    "url": "/build/static/js/main.fd8002e4.chunk.js"
+  },
+  {
+    "revision": "9db15f54bfbc231d5b19",
+    "url": "/build/static/js/runtime-main.496ae38d.js"
+  }
+]);
